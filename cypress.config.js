b95350b9579512b43c0e077388cd16e5b58b86ae
suvarnaghule <sghule@registrarcorp.com>
2024-11-19 21:00:12 +0530

    update files

--- cypress.config.js
+++ cypress.config.js
@@ -5,10 +5,20 @@ module.exports = defineConfig({
   chromeWebSecurity: false,
   defaultCommandTimeout: 30000,
   video:true,
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: "cypress/results", // Report directory based on environment variables
+    overwrite: false,
+    html: false,
+    json: false,
+    xml: true,
+    chart:true,
+  },
   e2e: {
     baseUrl:'https://monitor.dev.compliancemonitorapi.com',
     setupNodeEvents(on, config) {
       // implement node event listeners here'
+      require('cypress-mochawesome-reporter/plugin')(on);
       allureWriter(on, config);
       require('@cypress/grep/src/plugin')(config);
       return config;
