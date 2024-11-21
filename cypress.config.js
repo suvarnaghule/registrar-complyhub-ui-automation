9e274a630438ef8a41474c7784e4244bc63e4a92
suvarnaghule <sghule@registrarcorp.com>
2024-11-21 17:50:22 +0530

    update config

--- cypress.config.js
+++ cypress.config.js
@@ -7,15 +7,26 @@ module.exports = defineConfig({
   video:true,
   reporter: 'junit',
   reporterOptions: {
-    mochaFile: 'cypress/results/report.xml',
+    mochaFile: "cypress/results/report.xml",
     toConsole: true  
-  },
+  },  
   e2e: {
     baseUrl:'https://monitor.dev.compliancemonitorapi.com',
     setupNodeEvents(on, config) {
+      
       // implement node event listeners here'
      // require('cypress-mochawesome-reporter/plugin')(on);
      // allureWriter(on, config);
+  /*   on('before:spec', (spec) => {     
+      config.reporter = 'junit';
+      config.reporterOptions = {
+        mochaFile: `cypress/results/junit-${spec.name}.xml`, 
+        toConsole: true,
+        overwrite: false
+      };
+      //console.log(`Running spec file: ${spec.name}`);
+      return config;
+    }); */   
       require('@cypress/grep/src/plugin')(config);
       return config;
     },
