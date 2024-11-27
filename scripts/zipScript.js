da10cb609cdb2a188743cba9e6df48fd3397e451
suvarnaghule <sghule@registrarcorp.com>
2024-11-27 15:25:35 +0530

    add script file

--- scripts/zipScript.js
+++ scripts/zipScript.js
@@ -3,15 +3,15 @@ const AdmZip = require('adm-zip');
 // Create a new instance of AdmZip
 const zip = new AdmZip();
 
-const resultDir = './zipReport';
+const resultDir = 'cypress/reports/mochawesome-report';
       const fs = require('fs');
       if (!fs.existsSync(resultDir)) {
         console.log(`Directory ${resultDir} does not exist. Creating...`);
         fs.mkdirSync(resultDir, { recursive: true });
       } 
 // Specify the folder you want to zip and the output ZIP file
-const folderToZip = './allure-report';
-const outputZip = './zipReport/allure-report.zip';
+const folderToZip = 'cypress/reports/mochawesome-report/TestReport';
+const outputZip = 'cypress/reports/mochawesome-report/report.zip';
 
 // Add the folder contents to the zip file
 zip.addLocalFolder(folderToZip);
